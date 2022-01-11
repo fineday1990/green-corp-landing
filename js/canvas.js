5ce198254d3e571330f09665b52d4a24503b558f
Ilya Vishnevskiy <vishnyajudo@gmail.com>
2022-01-11 22:17:49 +0300

    new

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,4 @@
-const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"];
+const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81", "255, 195, 0"];
 const BUBBLE_DENSITY = 100;
 
 function generateDecimalBetween(left, right) {
