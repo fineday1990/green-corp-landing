270c50803dc171bcd544fccbc95b29f960115d4d
Ilya Vishnevskiy <vishnyajudo@gmail.com>
2022-01-24 10:52:49 +0300

    new

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,4 @@
+// динамическое изменение с 0 до 5000 с шагом 200
 const INCREASE_NUMBER_ANIMATION_SPEED = 150;
 
 function increaseNumberAnimationStep (i, element, endNumber) {
@@ -24,6 +25,7 @@ function initIncreaseNumberAnimation() {
 
 initIncreaseNumberAnimation();
 
+//изменение формы,"другое" -> дополнительная вкладка 
  document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
 	if (event.target.value === 'other') {
 	  const formContainer = document.createElement("div");
@@ -43,3 +45,32 @@ initIncreaseNumberAnimation();
 	document.querySelector('#form form').removeChild(otherInput); 
 	}
  });
+
+
+// изменение шапки
+function updateScroll() {
+	if (window.scrollY > 0) {
+		 document.querySelector('header').classList.add('header__scrolled');
+	} else {
+		 document.querySelector('header').classList.remove('header__scrolled');
+	}
+	let windowBottomPosition = window.scrollY + window.innerHeight;
+	let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+	if (windowBottomPosition >= countElementPosition && !animationInited) {
+		 animationInited = true;
+		 initIncreaseNumberAnimation();
+	}
+}
+window.addEventListener('scroll', updateScroll);
+function addSmoothScroll(anchor) {
+	anchor.addEventListener('click', function(e) {
+		 e.preventDefault();
+		 document.querySelector(this.getAttribute('href')).scrollIntoView({
+			  behavior: 'smooth'
+		 });
+	});
+}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	addSmoothScroll(anchor);
+});
+addSmoothScroll(document.querySelector('.more-button'));
